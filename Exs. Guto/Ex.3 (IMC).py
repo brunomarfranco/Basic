f0197aedd29fa20f8b15565f70082b5dd8d76004
Bruno Franco <brunomarfranco@gmail.com>
2022-04-01 20:54:17 -0300

    original

--- Exs. Guto/Ex.3 (IMC).py
+++ Exs. Guto/Ex.3 (IMC).py
@@ -4,7 +4,7 @@ IMC = weight / height**2
 
 if IMC < 18.5:
     print("Você está abaixo do peso ideal.")
-elif IMC < 25:
+elif 18.5 < IMC < 25:
     print("Parabéns! Você está no peso ideal!")
 elif IMC < 30:
     print("Você está com sobrepeso.")
