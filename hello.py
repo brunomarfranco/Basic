2e721d077e574671c6f8bafab67174969e979ac9
Bruno Franco <brunomarfranco@gmail.com>
2022-04-05 17:04:56 -0300

    added "Dictionaries" and 2nd try to ignore test.py

--- hello.py
+++ hello.py
@@ -162,3 +162,27 @@ def max_num(num1, num2, num3):
 
 
 print(max_num(3, 5, 4))
+
+print("\n")
+
+print("DICTIONARIES")
+
+monthConversions = {
+    "Jan": "January",
+    2: "February",
+    "Mar": "March",
+    "Apr": "April",
+    5: "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December",
+}
+
+print(monthConversions["Nov"])
+print(monthConversions.get("Dec"))
+print(monthConversions.get(2))
+print(monthConversions.get("Luv", "Not a valid key"))
