38d8d6b564d9b404c592cc8908b1db56787ff573
Bruno Franco <brunomarfranco@gmail.com>
2022-05-07 16:46:49 -0300

    git.ignore test

--- Exs. Guto/Ex.8 (Palindromos non-Py).py
+++ Exs. Guto/Ex.8 (Palindromos non-Py).py
@@ -7,7 +7,7 @@ processed_sentence = re.sub("[^a-z0-9]", "", processed_sentence)
 
 palindromo = True
 
-for left_index in range(len(processed_sentence) / 2):
+for left_index in range(len(processed_sentence) // 2):
     right_index = len(processed_sentence) - left_index - 1
     if processed_sentence[left_index] != processed_sentence[right_index]:
         palindromo = False
